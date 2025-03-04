73b7e0b17e3c31f4c5db43a81ea27a64c8d99771
Hager <hager1.essam@gmail.com>
2025-03-04 04:04:05 +0200

    DarkMode

--- bonesApp/src/app/app.component.ts
+++ bonesApp/src/app/app.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavBarComponent} from './layout/nav-bar/nav-bar.component';
 import {LoginComponent} from './features/auth/login/login.component';
 import {RegisterComponent} from './features/auth/register/register.component';
 import {ForgetPasswordComponent} from './features/auth/forget-password/forget-password.component';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, LoginComponent, RegisterComponent, ForgetPasswordComponent],
+  imports: [RouterOutlet, NavBarComponent, LoginComponent, RegisterComponent, ForgetPasswordComponent, MatToolbar, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'bonesApp';
+  isDarkMode = false;
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('darkMode', this.isDarkMode);
+    localStorage.setItem('darkMode', this.isDarkMode.toString());
+  }
+
+  ngOnInit() {
+    const savedMode = localStorage.getItem('darkMode') === 'true';
+    this.isDarkMode = savedMode;
+    document.body.classList.toggle('darkMode', savedMode);
+  }
+
 }
--- bonesApp/src/app/features/profile/profile.component.ts
+++ bonesApp/src/app/features/profile/profile.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
 
+  isDarkMode = false;
+  name ="PatientName";
+  email = "PatientEmail";
+  phoneNumber = "PhoneNumber";
+  address = "address";
+  age ="age";
+  gender ="Gender";
+
+  ngOnInit() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const savedMode = localStorage.getItem('darkMode');
+    this.isDarkMode = savedMode ? savedMode === 'true' : prefersDark;
+    this.applyTheme();
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    this.applyTheme();
+
+    localStorage.setItem('darkMode', this.isDarkMode.toString());
+  }
+
+  private applyTheme() {
+    document.body.classList.toggle('darkMode', this.isDarkMode);
+  }
 }
+
+
+
+
